b912a9141899333239967a73b8a739c3487ec2ea
zlwqa <asdori1369@gmail.com>
2020-11-29 22:38:39 +0400

    Рыба первого теста

--- src/test/java/tests/googleCalcTests.java
+++ src/test/java/tests/googleCalcTests.java
@@ -1,5 +1,18 @@
 package tests;
 
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class googleCalcTests {
+
+    @Test
+    public void test1() {
+        WebDriver driver = new ChromeDriver();
+        System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver.exe");
+        driver.get("https://www.google.ru/");
+        driver.quit();
+
+    }
 
 }

60270ed7260bca44d4b9db9501dc7f2dbeceb4d1
zlwqa <asdori1369@gmail.com>
2020-11-30 00:26:52 +0400

    Половина первого теста(нет)

---
+++ src/main/java/page/CalcGooglePage.java
@@ -0,0 +1,27 @@
+package page;
+
+public class CalcGooglePage {
+}
+//        driver.findElement(By.cssSelector("div[aria-label=\"открывающая скобка\"]")).click();
+//                driver.findElement(By.cssSelector("div[aria-label=\"синус\"]")).click();
+//                driver.findElement(By.cssSelector("div[aria-label=\"деление\"]")).click();
+//                driver.findElement(By.cssSelector("div[aria-label=\"умножение\"]")).click();
+//                driver.findElement(By.cssSelector("div[aria-label=\"Вычитание\"]")).click();
+//                driver.findElement(By.cssSelector("div[aria-label=\"сложение\"]")).click();
+//                driver.findElement(By.cssSelector("div[aria-label=\"равно\"]")).click();
+//                driver.findElement(By.cssSelector("div[aria-label=\"закрывающая скобка\"]")).click();
+//                driver.findElement(By.cssSelector("div[aria-label=\"открывающая скобка\"]")).click();
+//                //0
+//                driver.findElement(By.cssSelector("div [class=\"PaQdxb A2W7l\"] [jsname=\"bkEvMb\"]")).click();
+//                //1
+//                driver.findElement(By.cssSelector("div [class=\"PaQdxb A2W7l\"] [jsname=\"N10B9\"]")).click();
+//                //2
+//                driver.findElement(By.cssSelector("div [class=\"PaQdxb A2W7l\"] [jsname=\"lVjWed\"]")).click();
+//                //3
+//                driver.findElement(By.cssSelector("div [class=\"PaQdxb A2W7l\"] [jsname=\"KN1kY\"]")).click();
+//                //4
+//                driver.findElement(By.cssSelector("div [class=\"PaQdxb A2W7l\"] [jsname=\"xAP7E\"]")).click();
+//                //5
+//                driver.findElement(By.cssSelector("div [class=\"PaQdxb A2W7l\"] [jsname=\"Ax5wH\"]")).click();
+//                //6
+//                driver.findElement(By.cssSelector("div [class=\"PaQdxb A2W7l\"] [jsname=\"abcgof\"]")).click();
--- src/test/java/tests/googleCalcTests.java
+++ src/test/java/tests/googleCalcTests.java
@@ -1,17 +1,50 @@
 package tests;
 
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.junit.jupiter.api.*;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 
 public class googleCalcTests {
 
     @Test
     public void test1() {
-        WebDriver driver = new ChromeDriver();
         System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
         driver.get("https://www.google.ru/");
-        driver.quit();
+        driver.findElement(By.cssSelector("input.gLFyf.gsfi")).sendKeys("Калькулятор", Keys.ENTER);
+
+        driver.findElement(By.cssSelector("div[aria-label=\"открывающая скобка\"]")).click();
+        //1
+        driver.findElement(By.cssSelector("div [class=\"PaQdxb A2W7l\"] [jsname=\"N10B9\"]")).click();
+        driver.findElement(By.cssSelector("div[aria-label=\"сложение\"]")).click();
+        //2
+        driver.findElement(By.cssSelector("div [class=\"PaQdxb A2W7l\"] [jsname=\"lVjWed\"]")).click();
+        driver.findElement(By.cssSelector("div[aria-label=\"закрывающая скобка\"]")).click();
+        driver.findElement(By.cssSelector("div[aria-label=\"умножение\"]")).click();
+        //3
+        driver.findElement(By.cssSelector("div [class=\"PaQdxb A2W7l\"] [jsname=\"KN1kY\"]")).click();
+        driver.findElement(By.cssSelector("div[aria-label=\"вычитание\"]")).click();
+        //4
+        driver.findElement(By.cssSelector("div [class=\"PaQdxb A2W7l\"] [jsname=\"xAP7E\"]")).click();
+        //0
+        driver.findElement(By.cssSelector("div [class=\"PaQdxb A2W7l\"] [jsname=\"bkEvMb\"]")).click();
+        driver.findElement(By.cssSelector("div[aria-label=\"деление\"]")).click();
+        //5
+        driver.findElement(By.cssSelector("div [class=\"PaQdxb A2W7l\"] [jsname=\"Ax5wH\"]")).click();
+        driver.findElement(By.cssSelector("div[aria-label=\"равно\"]")).click();
+        assertEquals(1, driver.findElement(By.cssSelector("div [jsname=\"zLiRgc\"] [jsname=\"VssY5c\"]")).size());
 
     }
 

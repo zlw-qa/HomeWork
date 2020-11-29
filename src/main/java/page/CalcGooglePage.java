0b1896abd02aee4f0b4baafaefab37c327294774
zlwqa <asdori1369@gmail.com>
2020-11-30 01:48:21 +0400

    Написал FindBy в POM,выглядит костыльно, мб найду другое решение

--- src/main/java/page/CalcGooglePage.java
+++ src/main/java/page/CalcGooglePage.java
@@ -1,7 +1,63 @@
 package page;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+import static org.openqa.selenium.support.PageFactory.initElements;
+
 public class CalcGooglePage {
 
+    @FindBy(css = "input.gLFyf.gsfi")
+    public WebElement searchInput;
+
+    @FindBy(css = "div[aria-label=\"открывающая скобка\"]")
+    public WebElement openBracket;
+
+    @FindBy(css = "div[aria-label=\"закрывающая скобка\"]")
+    public WebElement closingBracket;
+
+    @FindBy(css = "div[aria-label=\"сложение\"]")
+    public WebElement plus;
+
+    @FindBy(css = "div[aria-label=\"вычитание\"]")
+    public WebElement minus;
+
+    @FindBy(css = "div[aria-label=\"умножение\"]")
+    public WebElement multiply;
+
+    @FindBy(css = "div[aria-label=\"деление\"]")
+    public WebElement division;
+
+    @FindBy(css = "div[aria-label=\"равно\"]")
+    public WebElement equally;
+
+    @FindBy(css = "div [class=\"PaQdxb A2W7l\"")
+    public WebElement one;
+
+    @FindBy(css = "div [class=\"PaQdxb A2W7l\"] [jsname=\"lVjWed\"]")
+    public WebElement two;
+
+    @FindBy(css = "div [class=\"PaQdxb A2W7l\"] [jsname=\"KN1kY\"]")
+    public WebElement three;
+
+    @FindBy(css = "div [class=\"PaQdxb A2W7l\"] [jsname=\"xAP7E\"]")
+    public WebElement four;
+
+    @FindBy(css = "div [class=\"PaQdxb A2W7l\"] [jsname=\"Ax5wH\"]")
+    public WebElement five;
+
+    @FindBy(css = "div [class=\"PaQdxb A2W7l\"] [jsname=\"abcgof\"]")
+    public WebElement six;
+
+    @FindBy(css = "div[aria-label=\"синус\"]")
+    public WebElement sin;
+
+
+    public CalcGooglePage(WebDriver driver) {
+        initElements(driver,     this);
+    }
 }
 //        driver.findElement(By.cssSelector("div[aria-label=\"открывающая скобка\"]")).click();
 //                driver.findElement(By.cssSelector("div[aria-label=\"синус\"]")).click();
